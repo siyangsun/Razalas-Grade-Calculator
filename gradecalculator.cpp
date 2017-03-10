d0da69ce3962dd487a10ee5aa8705c27d9c9c5b7
siyangsun <siyangsun2007@gmail.com>
2017-03-10 15:49:22 -0800

    Fixing compiler warnings

--- gradecalculator.cpp
+++ gradecalculator.cpp
@@ -57,56 +57,67 @@ void GradeCalculator::computeScore()
 void GradeCalculator::on_horizontalSlider_valueChanged(int unused)
 {
     computeScore();
+    Q_UNUSED(unused);
 }
 
 void GradeCalculator::on_horizontalSlider_2_valueChanged(int unused)
 {
     computeScore();
+    Q_UNUSED(unused);
 }
 
 void GradeCalculator::on_horizontalSlider_3_valueChanged(int unused)
 {
     computeScore();
+    Q_UNUSED(unused);
 }
 
 void GradeCalculator::on_horizontalSlider_4_valueChanged(int unused)
 {
     computeScore();
+    Q_UNUSED(unused);
 }
 
 void GradeCalculator::on_horizontalSlider_5_valueChanged(int unused)
 {
     computeScore();
+    Q_UNUSED(unused);
 }
 
 void GradeCalculator::on_horizontalSlider_6_valueChanged(int unused)
 {
     computeScore();
+    Q_UNUSED(unused);
 }
 
 void GradeCalculator::on_horizontalSlider_7_valueChanged(int unused)
 {
     computeScore();
+    Q_UNUSED(unused);
 }
 
 void GradeCalculator::on_horizontalSlider_8_valueChanged(int unused)
 {
     computeScore();
+    Q_UNUSED(unused);
 }
 
 void GradeCalculator::on_horizontalSlider_9_valueChanged(int unused)
 {
     computeScore();
+    Q_UNUSED(unused);
 }
 
 void GradeCalculator::on_horizontalSlider_10_valueChanged(int unused)
 {
     computeScore();
+    Q_UNUSED(unused);
 }
 
 void GradeCalculator::on_horizontalSlider_11_valueChanged(int unused)
 {
     computeScore();
+    Q_UNUSED(unused);
 }
 
 void GradeCalculator::on_radioButton_clicked()
